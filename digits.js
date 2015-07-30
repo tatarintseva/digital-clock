83690ac1e17cf083ab26066c287f87b195071a79
Olga Tatarintseva <olitatarintseva@gmail.com>
2015-07-30 19:49:37 +0300

    Digits of minutes value have been separated in two different blocks

--- digits.js
+++ digits.js
@@ -25,6 +25,41 @@ else if ((today.getHours() >= 20)){
     document.getElementById('hour-2').innerHTML = today.getHours() - 20;
 }
 
-document.getElementById('minute-1').innerHTML = today.getMinutes();
-document.getElementById('minute-2').innerHTML = today.getMinutes();
+if (today.getMinutes() < 10){
+    document.getElementById('minute-1').innerHTML = '0';
+}
+else if ((today.getMinutes() >= 10) && (today.getMinutes() < 20)){
+    document.getElementById('minute-1').innerHTML = '1';
+}
+else if ((today.getMinutes() >= 20) && (today.getMinutes() < 30)){
+    document.getElementById('minute-1').innerHTML = '2';
+}
+else if ((today.getMinutes() >= 30) && (today.getMinutes() < 40)){
+    document.getElementById('minute-1').innerHTML = '3';
+}
+else if ((today.getMinutes() >= 40) && (today.getMinutes() < 50)){
+    document.getElementById('minute-1').innerHTML = '4';
+}
+else if ((today.getMinutes() >= 50)){
+    document.getElementById('minute-1').innerHTML = '5';
+}
+
+if (today.getMinutes() < 10){
+    document.getElementById('minute-2').innerHTML = today.getMinutes();
+}
+else if ((today.getMinutes() >= 10) && (today.getMinutes() < 20)){
+    document.getElementById('minute-2').innerHTML = today.getMinutes() - 10;
+}
+else if ((today.getMinutes() >= 20) && (today.getMinutes() < 30)){
+    document.getElementById('minute-2').innerHTML = today.getMinutes() - 20;
+}
+else if ((today.getMinutes() >= 30) && (today.getMinutes() < 40)){
+    document.getElementById('minute-2').innerHTML = today.getMinutes() - 30;
+}
+else if ((today.getMinutes() >= 40) && (today.getMinutes() < 50)){
+    document.getElementById('minute-2').innerHTML = today.getMinutes() - 40;
+}
+else if ((today.getMinutes() >= 40) && (today.getMinutes() < 50)){
+    document.getElementById('minute-2').innerHTML = today.getMinutes() - 50;
+}
 console.log(today);
