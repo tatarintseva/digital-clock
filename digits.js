bb6bd9bc3c32d64475a36935746434865ea9b766
Olga Tatarintseva <olitatarintseva@gmail.com>
2015-08-01 17:34:37 +0300

    The date is real time is shown

--- digits.js
+++ digits.js
@@ -1,10 +1,18 @@
-var d = new Date();
+// var d = new Date();
 
-hours = d.getHours().toString().split('');
-minutes = d.getMinutes().toString().split('');
+function time(){
+    var d = new Date();
+    hours = ('0' + d.getHours()).slice(-2).toString().split('');
+    minutes = ('0' + d.getMinutes()).slice(-2).toString().split('');
 
-document.getElementById('hour-1').innerHTML = hours[0];
-document.getElementById('hour-2').innerHTML = hours[1];
+    document.getElementById('hour-1').innerHTML = hours[0];
+    document.getElementById('hour-2').innerHTML = hours[1];
 
-document.getElementById('minute-1').innerHTML = minutes[0];
-document.getElementById('minute-2').innerHTML = minutes[1];
+    document.getElementById('minute-1').innerHTML = minutes[0];
+    document.getElementById('minute-2').innerHTML = minutes[1];
+
+    console.log(d + d.getSeconds()); 
+}
+time();
+setInterval(function () {time()},1000);
+
