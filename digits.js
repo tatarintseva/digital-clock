0709fea59ad728dcc31142fed4af29dd4defc68e
Olga Tatarintseva <olitatarintseva@gmail.com>
2015-08-02 21:07:28 +0300

    Initial code for time in different time zones had been added

--- digits.js
+++ digits.js
@@ -1,8 +1,22 @@
 // var d = new Date();
+country = "Ukr";
+var ratio = 0
 
-function time(){
+if (country === "Ukr"){
+    ratio = 0;
+}
+else if (country === "Fr"){
+    ratio = -1;
+}
+else if (country === "Eng"){
+    ratio = -2;
+}
+
+function time(ratio){
     var d = new Date();
-    hours = ('0' + d.getHours()).slice(-2).toString().split('');
+
+    hours = d.getHours()+ratio;
+    hours = ('0' + hours).slice(-2).toString().split('');
     minutes = ('0' + d.getMinutes()).slice(-2).toString().split('');
 
     document.getElementById('hour-1').innerHTML = hours[0];
@@ -10,9 +24,7 @@ function time(){
 
     document.getElementById('minute-1').innerHTML = minutes[0];
     document.getElementById('minute-2').innerHTML = minutes[1];
-
-    console.log(d + d.getSeconds()); 
 }
-time();
-setInterval(function () {time()},1000);
+
+setInterval(function () {time(ratio)},0, 1000);
 
