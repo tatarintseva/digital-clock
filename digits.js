54b493594a839b59bac1c398a3fdcad4c427a831
Olga Tatarintseva <olitatarintseva@gmail.com>
2015-08-07 01:10:13 +0300

    Added timezones support draft

--- digits.js
+++ digits.js
@@ -1,19 +1,25 @@
-// var d = new Date();
-country = "Ukr";
-var ratio = 0
-
-if (country === "Ukr"){
-    ratio = 0;
-}
-else if (country === "Fr"){
-    ratio = -1;
-}
-else if (country === "Eng"){
-    ratio = -2;
+function country_selection(id){
+    console.log(id);
+    var ratio = 0;
+    if (document.getElementById('Ukr').checked){
+        ratio = 0;
+        console.log('Ukraine' + ratio);
+    }
+    else if (document.getElementById('Fr').checked){
+        ratio = -1;
+        console.log('France' + ratio);
+    }
+    else if (document.getElementById('Gb').checked){
+        ratio = -2;
+        console.log('Gb' + ratio);
+    }
+    setInterval(function () {time(ratio)},0, 100);    
 }
 
 function time(ratio){
     var d = new Date();
+
+    // console.log("ratio = " + ratio);
 
     hours = d.getHours()+ratio;
     hours = ('0' + hours).slice(-2).toString().split('');
@@ -24,7 +30,25 @@ function time(ratio){
 
     document.getElementById('minute-1').innerHTML = minutes[0];
     document.getElementById('minute-2').innerHTML = minutes[1];
+    return hours;
 }
 
-setInterval(function () {time(ratio)},0, 1000);
+function initial_country(){
+    return "Ukr";
+}
 
+// country_selection();
+
+function boot(){
+    console.log("boot")
+    var id = 'Gb'
+    var country_selectors = document.getElementsByClassName('country-selector');
+    console.log("here" + document.getElementsByClassName('country-selector').id)
+    for (var i=0;i<country_selectors.length;i++){
+        console.log("for in boot")
+        country_selectors[i].onclick = function (){console.log('a'); country_selection(id)};   
+    }
+}
+
+boot()
+//onchange = "country_selection(this.id);" 
